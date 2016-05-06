51f802822775cdb2305c664b193f216eb759eb3a
Cillion1 <cillionk@gmail.com>
2016-05-06 09:42:13 -0600

    Added files via upload

---
+++ Omok/src/Player.java
@@ -0,0 +1,13 @@
+/**
+ * Description: This class assigns the users game record 
+ * Author: Dennis Situ
+ * Last Updated: May 5, 2016
+ */
+
+public class Player {
+	String name;
+	int win;
+	int loss;
+	int total;
+	int winStreak;
+}
