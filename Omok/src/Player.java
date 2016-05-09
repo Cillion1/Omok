3a3c147a84cb0a5f1731a55eae6a10694adcdbd4
Cillion1 <cillionk@gmail.com>
2016-05-09 10:10:03 -0600

    Update Player.java

--- Omok/src/Player.java
+++ Omok/src/Player.java
@@ -5,9 +5,17 @@
  */
 
 public class Player {
-	String name;
-	int win;
-	int loss;
-	int total;
-	int winStreak;
+	String name = "";
+	int win = 0;
+	int loss = 0;
+	int total = 0;
+	int winStreak = 0;
+	
+	public void run() {
+		System.out.println("Player: " + name);
+		System.out.println("Wins: " + win);
+		System.out.println("Loss " + loss);
+		System.out.println("Total Games: " + total);
+		System.out.println("Win Streak " + winStreak);
+	}
 }
