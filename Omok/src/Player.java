9cba0503211a5b2ceb8f5452d509c4fad4edc1f3
Cillion1 <cillionk@gmail.com>
2016-05-10 11:46:14 -0600

    Update Player.java

--- Omok/src/Player.java
+++ Omok/src/Player.java
@@ -1,11 +1,17 @@
+import java.io.Serializable;
+
 /**
  * Description: This class assigns the users game record 
  * Author: Dennis Situ
  * Last Updated: May 5, 2016
  */
 
-public class Player {
-	String name = "";
+public class Player implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String name;
 	int win = 0;
 	int loss = 0;
 	int total = 0;
